bfcb5f68853972af5c86bf1d8b9632dcb41d42ef
Gautam Kumar <gautamk8749@gmail.com>
2026-08-20 04:55:09 +0530

    Update firebase-auth.js

--- assets/firebase-auth.js
+++ assets/firebase-auth.js
@@ -1,8 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  RecaptchaVerifier,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+  onAuthStateChanged,
+  updateProfile,
+  signInWithPhoneNumber
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyCR4mUxegTnOM2ECvaHkGbarEpneW4muYc",
+  authDomain: "all-free-image-tools.firebaseapp.com",
+  projectId: "all-free-image-tools",
+  storageBucket: "all-free-image-tools.firebasestorage.app",
+  messagingSenderId: "246434018521",
+  appId: "1:246434018521:web:e5ec006db60dbcaa9ffac8",
+  measurementId: "G-D77DYY38G6"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+window.AFITAuth = {
+  auth,
+  googleProvider,
+  RecaptchaVerifier,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+  onAuthStateChanged,
+  updateProfile,
+  signInWithPhoneNumber
 };
